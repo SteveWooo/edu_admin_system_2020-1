c413ec9a40cd39e8bdc8ccb53f870a9714c45e1a
stevewooo <461437874@qq.com>
2020-01-27 16:59:44 +0800

    add baseUrl

--- public/teacher/config.js
+++ public/teacher/config.js
@@ -1,7 +1,8 @@
 var swc = {
     config : {
         // baseUrl: "http://localhost:81/edms",
-        baseUrl: "https://www.deadfishcrypto.com/edms",
+        // baseUrl: "https://www.deadfishcrypto.com/edms",
+        baseUrl: "/edms",
         // 统一拉出框
         indexDrawer: [
         ]
