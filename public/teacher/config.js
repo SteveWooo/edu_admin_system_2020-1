698dfb2e5e8cb7c7b4113b4d4fda1ce509824758
stevewooo <461437874@qq.com>
2020-01-27 16:56:15 +0800

    a

--- public/teacher/config.js
+++ public/teacher/config.js
@@ -1,41 +1,9 @@
 var swc = {
     config : {
-        baseUrl: "http://localhost:81/edms",
+        // baseUrl: "http://localhost:81/edms",
+        baseUrl: "https://www.deadfishcrypto.com/edms",
         // 统一拉出框
         indexDrawer: [
-            { icon: 'mdi-contacts', text: 'Contacts' },
-            { icon: 'mdi-history', text: 'Frequently contacted' },
-            { icon: 'mdi-content-copy', text: 'Duplicates' },
-            {
-                icon: 'mdi-chevron-up',
-                'icon-alt': 'mdi-chevron-down',
-                text: 'Labels',
-                model: true,
-                children: [
-                    { icon: 'mdi-plus', text: 'Create label' },
-                ],
-            },
-            {
-                icon: 'mdi-chevron-up',
-                'icon-alt': 'mdi-chevron-down',
-                text: 'More',
-                model: false,
-                children: [
-                    { text: 'Import' },
-                    { text: 'Export' },
-                    { text: 'Print' },
-                    { text: 'Undo changes' },
-                    { text: 'Other contacts' },
-                ],
-            },
-            { icon: 'mdi-settings', text: 'Settings' },
-            { icon: 'mdi-message', text: 'Send feedback' },
-            { icon: 'mdi-help-circle', text: 'Help' },
-            { icon: 'mdi-cellphone-link', text: 'App downloads' },
-            { icon: 'mdi-keyboard', text: 'Go to the old version' },
         ]
-    },
-    utils : {
-        
     }
 }
